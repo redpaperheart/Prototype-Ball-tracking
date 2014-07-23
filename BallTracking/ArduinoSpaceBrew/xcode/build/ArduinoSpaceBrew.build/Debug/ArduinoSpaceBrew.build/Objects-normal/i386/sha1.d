8a22f5441b667fb3e36bf927b5ce735be3131884
adria <araidz@gmail.com>
2014-07-23 10:24:45 -0400

    deleted files affected by gitignore

--- BallTracking/ArduinoSpaceBrew/xcode/build/ArduinoSpaceBrew.build/Debug/ArduinoSpaceBrew.build/Objects-normal/i386/sha1.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/redpaperheart/Work/Projects/Prototypes/BallTracking/ArduinoSpaceBrew/blocks/Cinder-WebSocketPP/src/websocketpp/sha1/sha1.cpp \
-  /Users/redpaperheart/Work/Projects/Prototypes/BallTracking/ArduinoSpaceBrew/blocks/Cinder-WebSocketPP/src/websocketpp/sha1/sha1.h
